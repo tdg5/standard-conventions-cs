149f5210fea4fd2e30ffdf53795e45d08d5e5a1f
Danny Guinther <dannyguinther@gmail.com>
2024-11-06 22:15:15 -0500

    Reformat ruleset

--- Tdg5.StandardConventions.Tests/Data/StyleCopRules/EnabledRuleViolations.cs
+++ Tdg5.StandardConventions.Tests/Data/StyleCopRules/EnabledRuleViolations.cs
@@ -2,7 +2,6 @@ using Tdg5.StandardConventions.TestAnnotations;
 
 namespace Tdg5.StandardConventions.Tests.Data.StyleCopRules;
 
-[CodeAnalysisViolationExpected("CS1591", "Warning")]
 [CodeAnalysisViolationExpected("SA1600", "Warning")]
 public class EnabledRuleViolations
 {
