fd660576016404c169422eb346511b445a02f16f
Danny Guinther <dannyguinther@gmail.com>
2024-11-09 21:33:02 -0500

    Rename a couple of test files

--- Tdg5.StandardConventions.Tests/Data/SA0001/ProjectScopedExpectations.cs
+++ Tdg5.StandardConventions.Tests/Data/SA0001/ProjectScopedExpectations.cs
@@ -3,6 +3,6 @@ using Tdg5.StandardConventions.TestAnnotations;
 namespace Tdg5.StandardConventions.Tests.Data.SA0001;
 
 [ProjectAnalysisViolationExpected("SA0001", "Warning")]
-public class EnabledRuleViolations
+public class ProjectScopedExpectations
 {
 }
--- Tdg5.StandardConventions.Tests/Data/StyleCopRules/BlockScopedExpectations.cs
+++ Tdg5.StandardConventions.Tests/Data/StyleCopRules/BlockScopedExpectations.cs
@@ -3,6 +3,6 @@ using Tdg5.StandardConventions.TestAnnotations;
 namespace Tdg5.StandardConventions.Tests.Data.StyleCopRules;
 
 [CodeAnalysisViolationExpected("SA1600", "Warning")]
-public class EnabledRuleViolations
+public class BlockScopedExpectations
 {
 }
