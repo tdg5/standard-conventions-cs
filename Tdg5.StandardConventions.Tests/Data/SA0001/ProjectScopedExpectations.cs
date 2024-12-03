45554f08f38ef2681741dbe6314f2373263af26d
Danny Guinther <dannyguinther@gmail.com>
2024-12-03 08:27:05 -0800

    Upgrade packages to target net9.0

--- Tdg5.StandardConventions.Tests/Data/SA0001/ProjectScopedExpectations.cs
+++ Tdg5.StandardConventions.Tests/Data/SA0001/ProjectScopedExpectations.cs
@@ -6,6 +6,10 @@ namespace Tdg5.StandardConventions.Tests.Data.SA0001;
 /// The project should trigger a warning for SA0001 because the project file
 /// sets GenerateDocumentationFile to false.
 /// </summary>
+/// <remarks>
+/// Also triggers a warning for EnableGenerateDocumentationFile.
+/// </remarks>
+[ProjectAnalysisViolationExpected("EnableGenerateDocumentationFile", "Warning")]
 [ProjectAnalysisViolationExpected("SA0001", "Warning")]
 public class ProjectScopedExpectations
 {
