9c5cc020c988dd6f456753f881d2610f2a1c77d5
Danny Guinther <dannyguinther@gmail.com>
2024-12-05 12:58:00 -0800

    Make CI more robust to cached packages

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Standard Conventions for C#
 
 Library for enforcing my preferred conventions in C#.
+
+## Running Tests
+
+Test are run against packed versions of the library. Because of this, NuGet
+caching can get in the way of tests seeing updates. To get around run the
+following script to rebuild and run tests:
+
+```bash
+HARD=true ./scripts/clean-and-test.sh
+```
--- scripts/clean-and-test.sh
+++ scripts/clean-and-test.sh
@@ -3,7 +3,12 @@
 TEST_ARTIFACTS_PATH="$HOME/.cache/Tdg5.StandardConventions.Tests/test-artifacts"
 rm -rf "$TEST_ARTIFACTS_PATH"
 find . -type d \( -name bin -o -name obj \) -print0 | xargs -0 rm -rf
-[ -n "$HARD" ] && dotnet nuget locals all --clear
+
+if [ -n "$HARD" ]; then
+  # Try to preserve other caches to minimize restore time.
+  dotnet nuget locals global-packages --clear
+fi
+
 dotnet clean
 
 dotnet test
